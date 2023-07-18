121bc1c7b41dfef590f2e4370dc0b7f30b45ba07
sejalgadewal <sejal.gadewal@preciousinfosystem.com>
2023-07-18 12:07:39 +0530

    Add mailer for confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_115238) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_060734) do
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,12 +24,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_115238) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
-    t.string "type"
     t.string "name"
+    t.string "type"
+    t.string "role"
     t.string "username"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
