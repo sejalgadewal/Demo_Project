9051df19493b8776ecb4ea1e7e91d72e87883a47
Sejal <sejalgadewal25@gmail.com>
2023-07-23 23:16:14 +0530

    Lectures video uploaded and show to enrolled user

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,11 +1,27 @@
 class LecturesController < ApplicationController
+  #before_action :authenticate_user!
+
   def index
+    @course=Course.find(params[:course_id])
+    @lectures= @course.lectures
   end
 
   def new
+    @course=Course.find(params[:course_id])
+    @lecture=@course.lectures.build
   end
 
   def create
+    @course=Course.find(params[:course_id])
+    @lecture=@course.lectures.build(lecture_params)
+    @lecture.user = current_user
+
+    if @lecture.save
+      redirect_to course_lectures_path(@course), notice: 'Lecture was successfully created .'
+    else
+        render :new
+    end
+
   end
 
   def edit
@@ -15,8 +31,22 @@ class LecturesController < ApplicationController
   end
 
   def show
+    @course = Course.find(params[:course_id])
+    @lecture = @course.lectures.find(params[:id])
+    @quiz = @lecture.quiz
   end
 
   def destroy
+  
+  end
+
+  def mylecture
+    @course = Course.find(params[:course_id])
+    @lectures= @course.lectures
+  
+  end
+
+  def lecture_params
+    params.require(:lecture).permit(:video)
   end
 end
