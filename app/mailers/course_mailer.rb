3c94b01fca59e293ac083b73f2840c015aaabbde
sejalgadewal <sejal.gadewal@preciousinfosystem.com>
2023-07-25 19:22:55 +0530

    Add Course mailer for sending mail to user using letteropener

---
+++ app/mailers/course_mailer.rb
@@ -0,0 +1,8 @@
+class CourseMailer < ApplicationMailer
+    def send_lecture_email(student, course)
+      @student = student
+      @course = course
+      mail(to: @student.email, subject: "New Lecture Available in #{@course.title}")
+       
+    end
+end
