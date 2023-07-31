b645fae008d7ed93536a3cb57509d59108e40996
sejalgadewal <sejal.gadewal@preciousinfosystem.com>
2023-07-31 16:14:13 +0530

    Little change in mailer

--- app/mailers/course_mailer.rb
+++ app/mailers/course_mailer.rb
@@ -2,6 +2,6 @@ class CourseMailer < ApplicationMailer
   def send_lecture_email(student, course)
     @student = student
     @course = course
-    mail(to: @student.email, subject: "New Lecture Available in title :- #{@course.title} and description :- #{@course.description}")
+    mail(to: @student.email, subject: "New Lecture Available in title :- #{@course.title}")
   end
 end
